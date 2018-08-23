86a74616212e3e1aa8a8060a6bc434a3e91c4a31
Jennifer CHARLOIS <jcharloi@e1r3p9.42.fr>
2018-08-23 22:37:37 +0200

    starting routing

---
+++ connexion.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./login.css">
+        <meta charset = "utf-8">
+    </head>
+    <body>
+    <div>Pas encore inscrit(e) ?</div>
+    <input class="name" type="text" placeholder="Entrez votre nom">
+    <input type="button" placeholder="Se connecter">
+    
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./login.css">
+        <meta charset = "utf-8">
+    </head>
+    <body>
+    <!-- <div>Pas encore inscrit(e) ?</div>
+    <input class="name" type="text" placeholder="Entrez votre nom">
+    <input type="button" placeholder="Se connecter"> -->
+    <!-- <img class = "concept" src = "background.jpg"> -->
+        <div class="conteneur">
+            <div class="rond"><a class="text" href="connexion.php">Connecte toi !</a></div>
+            <div class="rond2"><a class="text" href="connexion.php">Pas encore inscrit(e) ?</a></div>
+        </div>
+    </body>
+</html>
